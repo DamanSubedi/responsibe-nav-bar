9b31310630b1c936e9262a0a53e1ad5aac4e9c50
Daman Subedi <ssssssss6943@gmail.com>
2024-01-23 12:43:42 +0545

    updated files

---
+++ java.js
@@ -0,0 +1,6 @@
+let menu = document.getElementById("menu");
+let menuBar = document.getElementById("menuBar");
+
+menuBar.addEventListener("click", function(){
+    menu.classList.toggle("show-Menu")
+})
